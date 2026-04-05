19dd7669a6570d42bd5957c556dda5eb84ffdec5
Victor-oss <victorio.lazaro15@gmail.com>
2026-04-05 14:56:06 -0300

    adicionando mapa das ruas do Gama e proximidades

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subject, takeUntil } from 'rxjs';
+import * as L from 'leaflet';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,15 +9,43 @@ import { Subject, takeUntil } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit, OnDestroy {
+  private map!: L.Map;
   private destroy$ = new Subject<void>();
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.initMap();
+    this.carregarRuas();
   }
 
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
+  }
+
+  initMap() {
+    this.map = L.map('map').setView([-16.0189, -48.0517], 15);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap'
+    }).addTo(this.map);
+  }
+
+  carregarRuas() {
+    this.http.get<any>('assets/roads.json')
+    .pipe(takeUntil(this.destroy$))
+    .subscribe(data => {
+      data.features.forEach((feature: any) => {
+        const coords = feature.geometry.coordinates;
+
+        const latlngs = coords.map((c: number[]) => [c[1], c[0]]);
+
+        L.polyline(latlngs, {
+          color: 'blue',
+          weight: 2
+        }).addTo(this.map);
+      });
+    });
   }
 }
